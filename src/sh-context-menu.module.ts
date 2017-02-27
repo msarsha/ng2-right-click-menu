0ff74d823a6dc1687399d41f2311cfa8e1f8ce56
Matan Sar-Shalom <smatan84@gmail.com>
2017-02-27 16:20:35 +0200

    removed unused component

--- src/sh-context-menu.module.ts
+++ src/sh-context-menu.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { ShContextMenuDirective } from "./sh-context-menu.directive";
-import { ShContextMenuItem } from "./sh-context-item.component";
 import { ShContextMenuComponent } from './sh-context-menu.component';
 
 @NgModule({
-  declarations: [ShContextMenuDirective, ShContextMenuComponent, ShContextMenuItem],
+  declarations: [ShContextMenuDirective, ShContextMenuComponent],
   exports: [ShContextMenuDirective],
   imports: [CommonModule],
   entryComponents: [ShContextMenuComponent]
